39e952b746fb6ff7b5e01b3149106ddcc5491bf3
Billy Clarke <wclarke@seas.upenn.edu>
2015-09-12 18:13:16 -0500

    Changed copyright notice

--- conf.php
+++ conf.php
@@ -8,7 +8,7 @@ if($_SESSION['s_auth'] != "Admin"){
 <div style="width: 97%;">
 		<p>
 			<b>Confidentiality Notice:</b><br />
-			The information on these pages is intended for <?php echo $out; ?> the SPEC Executive Board and the SPEC Advisors only. Distribution of this information to third-parties is strictly prohibited. If you are not an intended recipient, you must not copy, distribute or take any action in reliance on it. 
+			The information on these pages is intended for <?php echo $out; ?> the SPEC Executive Board and the SPEC Advisors only. Distribution of this information to third-parties is strictly prohibited. If you are not an intended recipient, you must not copy, distribute or take any action in reliance on it.
 		</p>
-		<small>Software developed by <a href="mailto:andrewjshults@gmail.com">Andrew Shults</a> ©2008</small>
+		<small>Software developed by <a href="mailto:andrewjshults@gmail.com">Andrew Shults</a> ©2008. Some parts developed by <a href="mailto:billyclarke94@gmail.com">Billy Clarke</a>, copyright 2013-2015</small>
 	</div>
