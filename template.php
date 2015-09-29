890bf6f9953d130e1dc1c1b887f737531e5fc4d6
Billy Clarke <wclarke@seas.upenn.edu>
2015-09-29 18:48:44 -0500

    Adds template.php as template for files with bootstrap_{head,tail}.php and navbar.php

---
+++ template.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php require("bootstrap_head.html") ?>
+    <title>Page Title</title>
+  </head>
+  <body>
+    <?php require("navbar.php") ?>
+
+    <?php require("bootstrap_foot.html") ?>
+  </body>
+</html>
