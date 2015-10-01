763e118fa92036a7565d910620ca02a396c7c5d4
Billy Clarke <wclarke@seas.upenn.edu>
2015-10-01 14:34:19 -0500

    Added committee select to login.php

--- login.php
+++ login.php
@@ -5,34 +5,45 @@ require_once("db.php");
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
-
+    <?php require("bootstrap_head.html") ?>
     <title>SPEC Budget Login</title>
+    <script>
+function setUserOptions(committee_id){
+  if (committee_id == "") {
+    document.getElementById("user_id").innerHTML = "";
+    return;
+  } else {
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+        document.getElementById("user_id").innerHTML = xmlhttp.responseText;
+      }
+    }
+    xmlhttp.open("GET","get_user_id_name_for_committee_id_as_options.php?committee_id="+committee_id,true);
+    xmlhttp.send();
+  }
+}
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    </script>
   </head>
-
   <body>
 
     <div class="container">
 
       <form action="login_handler.php" method="post" class="form-signin" style="max-width:300px; padding: 15px; margin: 0 auto;">
         <h2 class="form-signin-heading">Please sign in</h2>
-        <label for="user_id" class="sr-only">Name</label>
-        <select class="form-control" id="user_id" name="user_id">
-          <?php $users = get_user_id_name_for_committee_id($comittee_id);
-            foreach($users as $user){
-              $id = $user[0];
-              $name = $user[1];
+        <label for="committee_id" class="sr-only">Committee</label>
+        <select class="form-control" id="committee_id" name="committee_id" onchange="setUserOptions(this.value)">
+          <?php $committees = get_committees_id_name();
+            foreach($committees as $committee){
+              $id = $committee[0];
+              $name = $committee[1];
               echo'<option value="'.$id.'">'.$name.'</option>';
             }
           ?>
+        </select>
+        <label for="user_id" class="sr-only">Name</label>
+        <select class="form-control" id="user_id" name="user_id">
         </select>
         <label for="password" class="sr-only">Password</label>
         <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
@@ -40,5 +51,7 @@ require_once("db.php");
       </form>
 
     </div> <!-- /container -->
+
+    <?php require("bootstrap_foot.html") ?>
   </body>
 </html>
