038ca150d5c4cf0586cef11dec1b23f855fa98c4
Billy Clarke <wclarke@seas.upenn.edu>
2015-11-11 10:51:30 -0600

    Removes setting session variables for user_id and committee_id in login_handler

--- login_handler.php
+++ login_handler.php
@@ -6,12 +6,13 @@ if(!check_user_password($user_id, $_POST["password"])){
 	header("Location: ./?loginretry=y");
   die("Incorrect Password");
 }
+
+$_SESSION['s_user_id'] = $user_id;
+$_SESSION['s_committee_id'] = get_committee_id_for_user_id($user_id);
+
 if(md5($_POST["password"]) == md5("SPEC Events")){
 	header("Location: reset_password.php?name=".$_POST["name"]."&committee=".$_POST['auth']);
 }
-
-$_SESSION['s_user_id'] = $_POST['user_id'];
-$_SESSION['s_committee_id'] = get_committee_id_for_user_id($_SESSION['s_user_id']);
 
 	header("Location: ./");
 ?>
