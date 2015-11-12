8be1642c5c4fb8f342a11d33ed98e75b76c9b2e1
Billy Clarke <wclarke@seas.upenn.edu>
2015-11-12 10:16:02 -0600

    Adds error reporting on db failure

--- db.php
+++ db.php
@@ -4,6 +4,7 @@ require_once("dbpass.php");
 	$dbhost = 'localhost';
 	$dbuser = 'speceven_budget';
 	$dbname = "speceven_budget";
+  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	$dbConnect = ($GLOBALS["___mysqli_ston"] = mysqli_connect($dbhost,  $dbuser,  $dbpass, $dbname));
 	if (!$dbConnect) {
 	   die('Could not connect: ' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)));
