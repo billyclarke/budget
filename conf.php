4402aa5bdf397000df4b7a22c61aa90086945ac4
Billy Clarke <wclarke@seas.upenn.edu>
2016-04-03 22:44:28 -0500

    Updates copyright notice and changes copyright character to word

--- conf.php
+++ conf.php
@@ -10,5 +10,5 @@ if($_SESSION['s_auth'] != "Admin"){
 			<b>Confidentiality Notice:</b><br />
 			The information on these pages is intended for <?php echo $out; ?> the SPEC Executive Board and the SPEC Advisors only. Distribution of this information to third-parties is strictly prohibited. If you are not an intended recipient, you must not copy, distribute or take any action in reliance on it.
 		</p>
-		<small>Software developed by <a href="mailto:andrewjshults@gmail.com">Andrew Shults</a> ©2008. Some parts developed by <a href="mailto:billyclarke94@gmail.com">Billy Clarke</a>, copyright 2013-2015</small>
+		<small>Software developed by <a href="mailto:andrewjshults@gmail.com">Andrew Shults</a> copyright 2008. Some parts developed by <a href="mailto:billyclarke94@gmail.com">Billy Clarke</a>, copyright 2013-2016</small>
 	</div>
